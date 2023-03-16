317e74fff5fc2ebda9420e62eb6edcc25d13453f
svijjeswarapu@linkfields.com <vjsuresh20021@yahoo.co.in>
2023-03-16 17:28:57 +0530

    My second try

--- sampleproject/src/com/demo/gitex/GitDemo.java
+++ sampleproject/src/com/demo/gitex/GitDemo.java
@@ -5,6 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My First Git");
+System.out.println("My Second try");
 	}
 
 }
